cafb338c7d9b0a6fab84f8567defcb0cd6c5424a
Johnny Soong <johnnysoong@hotmail.com>
2018-08-04 01:47:04 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
   # your code here
+   new_hash = {}
+   arr = []
+   hash.each do |key, value|
+     style = key
+     arr << style
+     value.each do |key, value|
+       new_hash[key] = value
+       new_hash[key][:style] = [style]
+       if key == :javascript
+         new_hash[key][:style] = arr
+       end
+      end
+    end
+  new_hash
 end
